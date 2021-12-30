b190bf9d5efa645ffdfbd980d4d0dfa09daa6730
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2021-12-30 13:50:38 +0700

    Add material ui

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import {Typography} from '@mui/material'
+
+
+const App = () => {
+  return (
+    <Typography variant="h1">Hello World!</Typography>
+  )
+}
+
+export default App
